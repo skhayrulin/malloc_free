00429eca13afecee1c6a8c32b5635c780e02c75e
Sergey Khayrulin <s.khayrulin@gmail.com>
2017-11-25 19:30:05 +0700

    added makefile and ocl header files

--- ocl_tests/multi_dev_test/utils/ocl_solver.h
+++ ocl_tests/multi_dev_test/utils/ocl_solver.h
@@ -10,7 +10,7 @@
 #include <string>
 #include <vector>
 #if defined(__APPLE__) || defined(__MACOSX)
-#include "../inc/OpenCL/cl.hpp"
+#include "../OpenCL/cl.hpp"
 //	#include <OpenCL/cl_d3d10.h>
 #else
 #include <CL/cl.hpp>
